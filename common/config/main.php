fab96bea5277d41f2e486f7f1bcbdffbea7cb92d
Julian Ramirez <hipogea@hotmail.com>
2021-04-11 12:12:09 -0500

    modificaciones3

--- common/config/main.php
+++ common/config/main.php
@@ -128,15 +128,7 @@ return [
            ],
     
     'components' => [
-        'request' => [
-
-        	// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-
-        	'cookieValidationKey' => 'tuClave',
-
-        	'enableCsrfValidation' => true,
-
-], 
+        
            
         
 
