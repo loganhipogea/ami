36f805e9b45486637759225e5bf682b70362ebc8
hipogea <hipogea@hotmail.com>
2021-09-09 01:16:42 -0500

    wqwqs

--- frontend/modules/sigi/views/edificios/index.php
+++ frontend/modules/sigi/views/edificios/index.php
@@ -78,6 +78,9 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]); ?>
     <?php Pjax::end(); ?>
+        <?php
+        echo \onmotion\telegram\Telegram::widget();
+        ?>
 </div>
     </div>
 </div>
